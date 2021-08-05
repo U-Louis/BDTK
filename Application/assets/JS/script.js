a9222ea18a33253285e581ef67f89d75f3471559
Louis <louis.urbani@yahoo.com>
2021-08-05 11:07:33 +0200

    autocompletion engine OK

--- Application/assets/JS/script.js
+++ Application/assets/JS/script.js
@@ -5,7 +5,7 @@
  * IN:NONE
  * OUT:NONE
  * */
-function makeMenu(){
+/* function makeMenu(){
     var menu =["Catalogue", "Mon-compte", "Accueil"]
     var pageHTML = ["#","#","accueil-page.html"];
     var ul = document.createElement("ul");
@@ -30,5 +30,70 @@ function makeMenu(){
     }
 
 }
+makeMenu(); */
 
-makeMenu();
+
+
+/** === CATALOG === */
+const LASTINDEXOFALBUMS = 629;
+
+/** == Search bars == */
+
+/**Init */
+var searchBarSuggestionsBox = document.getElementById("searchBarSuggestionsBox");
+var searchBarInput = document.getElementById("searchBarInput");
+var searchButton =  document.getElementById("searchButton");
+var searchData = searchBarInput.value;
+var tempResults = [];
+
+
+/**
+ * adding an event (e) to the onkeyup event
+ * @param {String} letter typed in searchBarInput
+ */
+searchBarInput.onkeyup = (e) => {
+searchData = e.target.value; //filling this var with all letters typed
+autoCompletion(searchData, '5');
+}
+
+
+function autoCompletion(data, /*inputKey,*/ itemsReturned){
+//inputKeys : ref, titre, auteur, serie
+
+let found = false; //flag
+let k =0; //iterator for number of items to return
+
+for (let i =1; k < itemsReturned  ; i++){
+
+  if(i==LASTINDEXOFALBUMS+1){
+    break;
+  }  
+
+  let j = i.toString();
+  let target;
+
+  try {
+     target = albums.get(j).titre;
+  }
+  catch {
+    console.log("Ref. "+i+" doesn't exist.");
+    continue;
+  }
+  console.log("Ref. "+i+ " checked");
+    
+  if(target != undefined && target.indexOf(data) != -1 ){
+    found = true;
+    k++;
+    tempResults.push(albums.get(j));
+    //updateTempResultsBox();
+    console.log("found !");
+  }
+}
+if(!found){
+  console.log("no match");
+}
+}
+
+
+
+
