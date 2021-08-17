c2dc4f5faad6807dfc754874ea23c76c1bdfd7a6
Clydic <70671047+Clydic@users.noreply.github.com>
2021-08-17 15:49:27 +0200

    Mise en place du bouton deconnexion

--- Application/assets/JS/home.js
+++ Application/assets/JS/home.js
@@ -9,22 +9,40 @@
  * @returns Array
  */
 var id = function(){
-    var cookiePair = document.cookie.split("=");
+    var cookiePair = document.cookie.split("=") ;
     return cookiePair[1];
 }
+
+function getCookie(name){
+    var arrCookie = document.cookie.split("; ");
+    for (var i = 0; i<arrCookie.length; i++){
+        var cookiePair = arrCookie[i].split("=");
+        if (name == cookiePair[0]){
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+}
+
 /**
  * @function firstLetter
  */
 function firstLetter(){
     //Get the first name of the ID
-    var nameOfUser = users.get(id()).firstname;
+    var nameOfUser = users.get(getCookie("id")).firstname;
     // Search first letter of the first name
     var matches = nameOfUser.match(/\b(\w)/g);
     //Join the letter
     var acronym = matches.join("");
     //Write letter in a HTML balise
-    document.querySelector("h2").innerHTML = acronym;
+   
+    document.querySelector("#idutilisateur").innerHTML = acronym;
 }
 
 firstLetter();
+
+document.querySelector("#iddeco").addEventListener("click",function(){
+    document.cookie = "id=-100";
+    document.cookie = "conected=false";
+    localStorage.setItem("connected","false")
+})
 
