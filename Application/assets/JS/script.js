7a5cf856723c28a5edeb23cdeca138b7901fdda3
Clydic <70671047+Clydic@users.noreply.github.com>
2021-08-18 09:45:45 +0200

    Déplacement de la fonction getCoookie

--- Application/assets/JS/script.js
+++ Application/assets/JS/script.js
@@ -619,3 +619,18 @@ function redirectResearch(input, inputKey) {
             refSearchButton.click();
     }
 }
+
+/**Get the storage in a cookie and return the value of the id.
+ * @function getCookie
+ * @returns Array
+ */
+
+function getCookie(name){
+    var arrCookie = document.cookie.split("; ");
+    for (var i = 0; i<arrCookie.length; i++){
+        var cookiePair = arrCookie[i].split("=");
+        if (name == cookiePair[0]){
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+}
