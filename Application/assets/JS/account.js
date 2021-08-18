088d472c3833ca50ecc458fe6dca2bfd1ff0f736
Clydic <70671047+Clydic@users.noreply.github.com>
2021-08-18 14:33:51 +0200

    Affichage de la liste des livres Ecriture des commentaires

--- Application/assets/JS/account.js
+++ Application/assets/JS/account.js
@@ -1,63 +1,79 @@
 (function(){
+    // Initialisation
     var id = getCookie("id");
     var arrListofBook = users.get(id)["listofbook"]
+    var tbody = document.querySelector("#idemprunt table tbody")
+  
+    //Traitment
+    //Display a personalize welcoming message
     welcomeMsg(id);
+    // Write informations of the user
     showInfo(id);
+    // Write a msg if the user is an Adherent
     contributionMsg(id);
-    showListOfBook(arrListofBook);
+    // Display the list of book borrowed if the user is an Adherent 
+    listOfBook(tbody,arrListofBook);
 }());
 
+/**Write the name of user in the header
+ * @function welcomeMsg
+ * @param {String} id 
+ */
 function welcomeMsg(id){
     document.querySelector("#titlename").innerHTML = users.get(id)["firstname"];
 }
 
+/**Write information  in a table
+ * @function showInfo
+ * @param {String} id 
+ */
 function showInfo(id){
     var arrInfo = ["name", "firstname" , "dateofbirth" ,
     "address1" , "address2"  ,"postalcode" , "city" , 
     "email" , "mobilenumber" , "homenumber"];
-    
-    for(i in arrInfo){
-        let thAct = "th" + arrInfo[i];
-        let info = arrInfo[i]
-        if(users.get(id)[info] == ""){
-            document.querySelector("#"+thAct).innerHTML =  " ";
-        }else{
-            document.querySelector("#"+thAct).innerHTML =  users.get(id)[info];
-        }
+    document.querySelector("#thid").innerHTML =  id;
+    for(index in arrInfo){
+        // We get the id of each row we need
+        let thAct = "th" + arrInfo[index];
+        // We get the key of info we need
+        let info = arrInfo[index]
+        // We write the value of info we got in the cell
+        document.querySelector("#"+thAct).innerHTML =  users.get(id)[info];
         
     }
 }
 
+/**Display a message if the user is an adherent informing him if his contribution is updated.
+ * @function contributionMsg
+ * @param {String} id 
+ */
 function contributionMsg(id){
+    //We get the status of his contribution
     var isUpdate = users.get(id)["status"].get("isUpdated");
+    //We get the satus of adherent in order to control if the user is an adherent
     var isAdherent = users.get(id)["status"].get("hasRightAdherent")
-    var spanContribution =  document.querySelector("#spancontribution");
-    var divContribution = document.querySelector("#divcontribution")
+    var spanContribution =  document.querySelector("#spancontribution");//span where the message will be written
+    var divContribution = document.querySelector("#divcontribution");//di where is the span of message
+    //We control the status of user
     if (isUpdate == "true" && isAdherent == "true"){
-        spanContribution.innerHTML = "Votre cotisation est à jour.";
-        spanContribution.classList.add("bg-success");
+        writeMsgWithBg(spanContribution,"Votre cotisation est à jour.","bg-success");
         divContribution.classList.replace("d-none","d-block")
     }else if (isUpdate == "false" && isAdherent == "true"){
-        spanContribution.innerHTML = "Votre cotisation n'est pas à jour.";
-        spanContribution.classList.add("bg-danger");
+        writeMsgWithBg(spanContribution,"Votre cotisation n'est pas à jour.","bg-danger");
         divContribution.classList.replace("d-none","d-block")
-    }
-}
-
-function showListOfBook(list){
-    var tbody = document.querySelector("#idemprunt table tbody")
-    for(index in list){
-        var tr = document.createElement("tr");
-        tr.innerHTML = "test";
-        tbody.appendChild(tr);
     }
     
 }
+/**
+ * 
+ * @param {String} selector query selector of HTMLElement where the message is written
+ * @param {String} msg 
+ * @param {String} bootstrapcolor The bootstrap class background color of the HTMLElement
+ */
+function writeMsgWithBg(selector, msg, bootstrapcolor){
+        selector.innerHTML = msg;
+        selector.classList.add(bootstrapcolor);
+        
+}
 
-/* <tbody>
-<tr scope="row">
-<td id="#">439</td>
-<td>Montespa</td>
-<td>06/12/2021</td>
-</tr>
-</tbody> */
+
--- Application/assets/JS/script.js
+++ Application/assets/JS/script.js
@@ -58,4 +58,63 @@ function getCookie(name){
         }
     }
 }
+/*-------------------List of Book-------------------------- */
+/**
+ * @function listOfBook
+ * @param {Balise} tbody 
+ * @param {Array} array 
+ * 
+ */
+function listOfBook(tbody,array){
+    // We brow the Array array
+    for(var index in array){
+        // Initialisation
+        //We create HTMLElement tr
+        var tr = document.createElement("tr");
+        var titleOfAlbum = albums.get(array[index])["titre"];
+        //We append tr in a HTMLElement tbody
+        tbody.appendChild(tr);
+        //On create attribute scope and set it with row
+        tr.setAttribute("scope","row");
+        // We call functions makeTdCell who create cell with ref
+        makeTdCell(tr,array[index]);
+        // We call functions makeTdCell who create cell with Title of album
+        makeTdCell(tr,titleOfAlbum);
+        //We put in date a full date
+        var date = fullDate();
+        // We call functions makeTdCell who create cell with date
+        makeTdCell(tr,date);
+    }
+    
+}
 
+/**Create an object date and return a string date with format dd/mm/yyyy
+ * @function fullDate
+ * @returns String
+ */
+function fullDate(){
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    // We control the number of digit in the day
+    if (day<10){
+        day="0"+day;
+    }
+    // We control the number of digit in the month
+    if (month<10){
+        month="0"+month;
+    }
+    return day+"/"+month+"/"+date.getFullYear();
+
+}
+
+/**
+ * 
+ * @param {HTMLElement} node 
+ * @param {Any} data 
+ */
+function makeTdCell(node,data){
+    var td = document.createElement("td");
+        td.innerHTML = data;
+        node.appendChild(td);
+}
