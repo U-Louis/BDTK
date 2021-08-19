8e531a2f144ed4f20573cca62344dd9fe773f4df
Clydic <70671047+Clydic@users.noreply.github.com>
2021-08-19 13:28:17 +0200

    Control des pages d'utilisateurs

--- Application/assets/JS/home.js
+++ Application/assets/JS/home.js
@@ -1,46 +1,46 @@
-// var firstName = $('#firstName').text();
-// var lastName = $('#lastName').text();
-// var intials = $('#firstName').text().charAt(0) + $('#lastName').text().charAt(0);
-// var profileImage = $('#profileImage').text(intials);
+if ((!getCookie("connected") == "false") && (!getCookie("connected") == undefined)) {
+    (function() {
+        var id = getCookie("id");
 
-(function() {
-    var id = getCookie("id");
-
-    var isBibliothecaire = users.get(id)["status"].get("hasRightBibliothecaire");
-    var isGestionnaire = users.get(id)["status"].get("hasRightGestionnaire");
-    var isResponsable = users.get(id)["status"].get("hasRightResponsable");
-    var isAdmin = users.get(id)["status"].get("hasRightAdmin");
+        var isBibliothecaire = users.get(id)["status"].get("hasRightBibliothecaire");
+        var isGestionnaire = users.get(id)["status"].get("hasRightGestionnaire");
+        var isResponsable = users.get(id)["status"].get("hasRightResponsable");
+        var isAdmin = users.get(id)["status"].get("hasRightAdmin");
 
 
-    personalizeTitle(getCookie("id"), document.querySelector(".titlename"));
+        personalizeTitle(getCookie("id"), document.querySelector(".titlename"));
 
-    firstLetter(id, document.querySelector("#idutilisateur"));
+        firstLetter(id, document.querySelector("#idutilisateur"));
 
-    // Hide buttons of librarian if the user is not a librarian
-    if (!isBibliothecaire) {
-        document.querySelector("#divbibliothecaire").classList.add("d-none");
+        // Hide buttons of librarian if the user is not a librarian
+        if (!isBibliothecaire) {
+            document.querySelector("#divbibliothecaire").classList.add("d-none");
 
-    }
-    // Hide buttons of gestionnaire if the user is not a gestionnaire
-    if (!isGestionnaire) {
-        document.querySelector("#divgestionnaire").classList.add("d-none");
+        }
+        // Hide buttons of gestionnaire if the user is not a gestionnaire
+        if (!isGestionnaire) {
+            document.querySelector("#divgestionnaire").classList.add("d-none");
 
-    }
-    // Hide buttons of manager if the user is not a manager
-    if (!isResponsable) {
-        document.querySelector("#divresponsable").classList.add("d-none");
+        }
+        // Hide buttons of manager if the user is not a manager
+        if (!isResponsable) {
+            document.querySelector("#divresponsable").classList.add("d-none");
 
 
-    }
-    // Hide buttons of admininistrator if the user is not an Admin
-    if (!isAdmin) {
-        document.querySelector("#divadmin").classList.add("d-none");
+        }
+        // Hide buttons of admininistrator if the user is not an Admin
+        if (!isAdmin) {
+            document.querySelector("#divadmin").classList.add("d-none");
 
-    }
-    document.querySelector("#iddeco").addEventListener("click", function() {
-        logOut();
-    })
-}())
+        }
+        document.querySelector("#iddeco").addEventListener("click", function() {
+            logOut();
+        })
+    }())
+} else {
+    controlConnected();
+}
+
 
 
 //This function is in script.js file
