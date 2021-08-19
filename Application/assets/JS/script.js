7f7646ed49cbf9943a48167919e052456acb6a79
Clydic <70671047+Clydic@users.noreply.github.com>
2021-08-19 10:37:33 +0200

    Uniformisation des pages

--- Application/assets/JS/script.js
+++ Application/assets/JS/script.js
@@ -20,9 +20,9 @@ function getCookie(name) {
  * @param {Array} array 
  * 
  */
-function listOfBook(tbody,array){
+function listOfBook(tbody, array) {
     // We brow the Array array
-    for(var index in array){
+    for (var index in array) {
         // Initialisation
         //We create HTMLElement tr
         var tr = document.createElement("tr");
@@ -30,36 +30,36 @@ function listOfBook(tbody,array){
         //We append tr in a HTMLElement tbody
         tbody.appendChild(tr);
         //On create attribute scope and set it with row
-        tr.setAttribute("scope","row");
+        tr.setAttribute("scope", "row");
         // We call functions makeTdCell who create cell with ref
-        makeTdCell(tr,array[index]);
+        makeTdCell(tr, array[index]);
         // We call functions makeTdCell who create cell with Title of album
-        makeTdCell(tr,titleOfAlbum);
+        makeTdCell(tr, titleOfAlbum);
         //We put in date a full date
         var date = fullDate();
         // We call functions makeTdCell who create cell with date
-        makeTdCell(tr,date);
+        makeTdCell(tr, date);
     }
-    
+
 }
 
 /**Create an object date and return a string date with format dd/mm/yyyy
  * @function fullDate
  * @returns String
  */
-function fullDate(){
+function fullDate() {
     var date = new Date();
     var day = date.getDate();
     var month = date.getMonth();
     // We control the number of digit in the day
-    if (day<10){
-        day="0"+day;
+    if (day < 10) {
+        day = "0" + day;
     }
     // We control the number of digit in the month
-    if (month<10){
-        month="0"+month;
+    if (month < 10) {
+        month = "0" + month;
     }
-    return day+"/"+month+"/"+date.getFullYear();
+    return day + "/" + month + "/" + date.getFullYear();
 
 }
 
@@ -68,11 +68,28 @@ function fullDate(){
  * @param {HTMLElement} node 
  * @param {Any} data 
  */
-function makeTdCell(node,data){
+function makeTdCell(node, data) {
     var td = document.createElement("td");
-        td.innerHTML = data;
-        node.appendChild(td);
+    td.innerHTML = data;
+    node.appendChild(td);
 }
+/**-------------------------First letter display------------------------- */
+/**
+ * @function firstLetter
+ * @param {HTMLElement} selector
+ */
+function firstLetter(id, selector) {
+    //Get the first name of the ID
+    var nameOfUser = users.get(id).firstname;
+    // Search first letter of the first name
+    var matches = nameOfUser.match(/\b(\w)/g);
+    //Join the letter
+    var acronym = matches.join("");
+    //Write letter in a HTML balise
 
-
-
+    selector.innerHTML = acronym;
+}
+/*-------------------------------Display the first name of user.---------------------------------- */
+function personalizeTitle(id, selector) {
+    selector.innerHTML = users.get(id)["firstname"];
+}
